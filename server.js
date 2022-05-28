c9d905a4fe6f99e6da7a17c7a64cf14b4d1982e1
rominarotela <105689160+rominarotela@users.noreply.github.com>
2022-05-28 09:06:10 -0400

    Iniciando proyecto
    Se cargan los archivos previamente creados

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const hbs = require("express-handlebars");
+const app = express();
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+var corsOptions = {
+    origin: "http://localhost:9091"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// css
+app.use(express.static('public'));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({message: "Bienvenido Node backend 2020"});
+});
+
+// new route
+app.engine('.hbs', hbs.engine({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+
+app.set('view engine', '.hbs');
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/list_restaurantes', (req, res) => {
+    res.render('list_restaurantes')
+})
+
+app.get('/list_mesas', (req, res) => {
+    res.render('list_mesas')
+})
+
+app.post('/home', (req, res) => {
+    res.render('home')
+    console.log("home")
+})
+
+require("./app/routes/cliente.routes")(app);
+require("./app/routes/restaurante.routes")(app);
+require("./app/routes/mesa.routes")(app);
+require("./app/routes/reserva.routes")(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 9090;
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en puerto 9090.');
+});
