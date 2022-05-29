d66be21a88c3e9ad4f6faf4e6afdb1791313faac
rominarotela <romy_rotela@fpuna.edu.py>
2022-05-28 23:11:26 -0400

    creando ruta para buscar cliente

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -1,12 +1,15 @@
 // Aqui se agrega todas las rutas a las vistas utilizadas
 const restaurante = require("../controllers/restaurantedao.controller");
 const mesa = require("../controllers/mesadao.controller");
+const reserva = require("../controllers/reservadao.controller");
+const cliente = require("../controllers/clientedao.controller");
 module.exports = app => {
-    const restaurante = require("../controllers/restaurantedao.controller.js");
+    // const restaurante = require("../controllers/restaurantedao.controller.js");
     var router = require("express").Router();
-    router.post("/", restaurante.create);
-    router.get("/", restaurante.findAll);
+    // router.post("/", restaurante.create);
+    // router.get("/", restaurante.findAll);
     app.use('/reservar_restaurante', router);
     router.get("/:restaurante", mesa.findMesaByRestaurante);
+    router.get("/:restaurante/:mesa", cliente.findOne);
     app.use('/index', router);
 };
